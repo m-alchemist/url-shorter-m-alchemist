3fc93ae7df2fc1e95b0c88b993dd37ef5e357ecb
Mohamed Al-khamees <mohamedal0khamees@MOHAMEDs-MacBook-Pro.local>
2017-03-21 11:31:31 -0700

    fixing connection with mlab

--- app.js
+++ app.js
@@ -17,16 +17,17 @@ var port = process.env.PORT || 3000;
 var baseUrl = process.env.BASE_URL || ('http://localhost:' + port + '/');
 var urlConnection="mongodb://heroku_sm56z7t4:i020hsal10n52p54qq1vpck791@ds133340.mlab.com:33340/heroku_sm56z7t4";
 
+
 mongoose.connect(urlConnection);
 mongoose.connection.on('error', function(err) {
         console.error('MongoDB connection error: ' + err);
         process.exit(-1);
     }
 );
-app.listen(3000, ()=>{
-console.log('Running on port 3000');
-
-});
+// app.listen(3000, ()=>{
+// console.log('Running on port 3000');
+//
+// });
 
 //watch for incoming req to the route
 //http://localhost:3050/api
